637c46beea8c889ddb2bab347a7d28b7d57a2f91
JordyAraujo <jordyaraujo@outlook.com>
2020-10-07 16:19:05 -0300

    Lista de imagens

--- galeriaSM/routes.py
+++ galeriaSM/routes.py
@@ -4,5 +4,5 @@ from galeriaSM import app
 @app.route('/')
 @app.route('/index')
 def index():
-    users = {'Maria', 'Miguel', 'João', 'Amanda'}
-    return render_template('index.html', title='Galeria', users=users)
+    pics = {'https://assets.vogue.com/photos/5dfbb1d1e3e1b20009a63674/16:9/w_1200%2Cc_limit/e%26t-wedding-523.jpg', 'https://i.insider.com/5e2762f7ab49fd1ed25a9dc4?width=1100&format=jpeg&auto=webp', 'https://cdn0.weddingwire.com/articles/images/5/8/9/5/img_5985/t30_rw-plan-a-wedding-step-by-step-porterhouse-los-angeles.jpeg', 'https://images.squarespace-cdn.com/content/5e8734078136f513e25d34c4/1586886099769-FUIR9U8C5M6U274RA4H8/Beach+Wedding+Crim+Barefoot+Beach+Bride+%282%29.jpg?content-type=image%2Fjpeg'}
+    return render_template('index.html', title='Galeria', pics=pics)
