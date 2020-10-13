2550976f6402c3715920e01682059dd63bac52fa
JordyAraujo <jordyaraujo@outlook.com>
2020-10-13 19:36:46 -0300

    Tela de login, upload, seleção e fluxos OK

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
+import os
+
+
 class Config(object):
     ALLOWED_EXTENSIONS = ["JPEG", "JPG", "PNG"]
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'galeriasm'
--- galeriaSM/routes.py
+++ galeriaSM/routes.py
@@ -1,11 +1,26 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, flash, request, redirect, url_for
 from galeriaSM import app
 from werkzeug.utils import secure_filename
+from galeriaSM.login import LoginForm
 import os
 import boto3
 
 s3 = boto3.client('s3')
 bucket = 'galeriasmbucket'
+
+
+class Files():
+
+    filenames = []
+
+    def get_filenames(self):
+        return self.filenames
+
+    def set_filenames(self, names):
+        self.filenames = names
+
+
+f = Files()
 
 
 def allowed_image(filename):
@@ -24,44 +39,15 @@ def allowed_image(filename):
 @app.route("/")
 @app.route("/index", methods=["GET", "POST"])
 def index():
-    if request.method == "POST":
-        if request.files:
-            image = request.files["image"]
-            if image.filename == "":
-                print("No filename")
-                return redirect(url_for('index'))
-            if allowed_image(image.filename):
-                filename = secure_filename(image.filename)
-                s3.upload_fileobj(image, 'galeriasmbucket', filename)
-                print("Imagem salva")
-                return redirect(url_for('aprovacao'))
-            else:
-                print("Formato de arquivo não permitido")
-                return redirect(redirect.url)
-    return render_template("index.html")
-
-
-@app.route("/aprovacao", methods=["GET", "POST"])
-def aprovacao():
-    files = []
-    names = []
-    for obj in s3.list_objects_v2(Bucket=bucket)['Contents']:
-        url = s3.generate_presigned_url('get_object',
-                                        Params={
-                                            'Bucket': bucket,
-                                            'Key': obj['Key']
-                                        },
-                                        ExpiresIn=3600)
-        files.append(url)
-        names.append(obj['Key'])
-    return render_template('aprovacao.html', title='Aprovação', files=files, names=names)
-
-@app.route("/galeria", methods=["GET", "POST"])
-def galeria():
     files = []
     if request.method == "POST":
-        filenames = request.form.getlist('image')
-    for obj in filenames:
+        selected = request.form.getlist('image')
+        f.set_filenames(selected)
+    else:
+        selected = f.get_filenames()
+    if not selected:
+        flash("Nada a mostrar")
+    for obj in selected:
         url = s3.generate_presigned_url('get_object',
                                         Params={
                                             'Bucket': bucket,
@@ -69,4 +55,49 @@ def galeria():
                                         },
                                         ExpiresIn=3600)
         files.append(url)
-    return render_template('galeria.html', files=files)
+    return render_template('index.html', files=files)
+
+
+@app.route("/upload", methods=["GET", "POST"])
+def upload():
+    if request.method == "POST":
+        if request.files:
+            image = request.files["image"]
+            if image.filename == "":
+                flash('No filename')
+                return redirect(url_for('upload'))
+            if allowed_image(image.filename):
+                filename = secure_filename(image.filename)
+                s3.upload_fileobj(image, 'galeriasmbucket', filename)
+                flash("Imagem salva")
+                return redirect(url_for('index'))
+            else:
+                flash("Formato de arquivo não permitido")
+                return redirect(url_for('upload'))
+    return render_template("upload.html")
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash("Seja bem vindo, {}!".format(
+            form.username.data))
+        return redirect('/aprovacao')
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route("/aprovacao", methods=["GET", "POST"])
+def aprovacao():
+    files = {}
+    cont = 0
+    for obj in s3.list_objects_v2(Bucket=bucket)['Contents']:
+        url = s3.generate_presigned_url('get_object',
+                                        Params={
+                                            'Bucket': bucket,
+                                            'Key': obj['Key']
+                                        },
+                                        ExpiresIn=3600)
+        files[cont] = {'name': obj['Key'], 'url': url}
+        cont = cont + 1
+    return render_template('aprovacao.html', title='Aprovação', files=files)
